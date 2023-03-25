4ecbf2dc79af5ae4ac812ed0926154f33a104729
hohenheim99 <nicolas.calderon1@mail.udp.cl>
2023-03-25 18:01:28 -0300

    commit

--- clean_bch.py
+++ clean_bch.py
@@ -1,11 +1,11 @@
 import os
 
-path=input("folder: ")
+path="benchs_v"
 os.system("mkdir clean_benchs")
 for root, dirs, files in os.walk(path):
     for file in files:
-        with open(file, 'r') as infile, \
-            open("clean_benchs/"+file, 'w') as outfile:
+        with open(file, 'r') as infile:
+            with open("clean_benchs/"+file, 'w') as outfile:
                 data = infile.read()
                 data = data.replace("(", "")
                 data = data.replace(")","")
