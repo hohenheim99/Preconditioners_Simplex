e8cda48338d0fe2ff582b1190ce68b6c18dfe468
hohenheim99 <nicolas.calderon1@mail.udp.cl>
2023-04-27 12:01:28 -0400

    commit

--- model_test.py
+++ model_test.py
@@ -6,24 +6,25 @@ from keras.models import load_model
 from tensorflow import keras
 
 
-model = load_model('my_model.h5') # load model
+def predict_with_model(model_path,path_results,samples):
 
-path='test_results'  #path to the file
-n=1 #samples in the dataset
+    model = load_model(model_path) # load model
 
-input=read_data_AXB_n(path,n)
+    samples=1 #samples in the dataset
 
-input=padd_to_n(input,295) #ANN its trained with an input of 295-dimension, put this format everything with the zeroes pad function
+    input=read_data_AXB_n(path_results,samples)
 
-
-
-input=normalize(input) #Normalize input, with L2 norm (Note to me: try to replicated manually)
+    input=padd_to_n(input,295) #ANN its trained with an input of 295-dimension, put this format everything with the zeroes pad function
 
 
-ynew = model.predict(input) # prediction using the ANN
 
-for i in ynew:
-    if i > 0.5:
-        print('Applied simplex')
-    else:
-        print('No applied simplex')
+    input=normalize(input) #Normalize input, with L2 norm (Note to me: try to replicated manually)
+
+
+    ynew = model.predict(input) # prediction using the ANN
+
+    for i in ynew:
+        if i > 0.5:
+            return 1
+        else:
+            return 0
