ff5486fc3dcc4c4927c4c5ff2ab6e491a0312323
hohenheim99 <nicolas.calderon1@mail.udp.cl>
2023-07-03 02:29:57 -0400

    commit

--- data_analysis.py
+++ data_analysis.py
@@ -2,26 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-pd.options.display.float_format = '{:.3f}'.format
-Criteria = pd.read_csv('/home/nico/codes/benchmarks/MC-2/benchmark_model_mc1_2m_threshold_05.csv').replace(np.inf,np.nan, regex=True)
-Default=pd.read_csv('/home/nico/codes/benchmarks/base/benchmark_Default.csv').replace(np.inf,np.nan, regex=True)
+pd.options.display.float_format = '{:.4f}'.format
+Default=pd.read_csv('/home/nico/codes/benchmarks/base/benchmark_Default.csv')
+Criteria = pd.read_csv('/home/nico/codes/benchmarks/base/benchmark_Model_default_v2.csv')
 Criteria['File'] = Criteria['File'].str.replace('for_dataset/', '')
 Default['File'] = Default['File'].str.replace('for_dataset/', '')
 Default_grouped=Default.groupby('File')
 Criteria_grouped =Criteria.groupby('File')
 
-col=['Time', 'Nodes','ANN time']
+# col=['Time', 'Nodes','ANN time']
 
-df1 = Default_grouped[col].mean()
-df2 = Criteria_grouped[col].mean()
+# df1 = Default_grouped[col].mean()
+# df2 = Criteria_grouped[col].mean()
 
-df_combined = pd.concat([df1.add_suffix('_Default'), df2.add_suffix('_Modelo')], axis=1)
-desired_order = ['Time_Default', 'Time_Modelo','Time_Modelo without ANN', 'Nodes_Default','Nodes_Modelo','ANN time_Default','ANN time_Modelo']
-df_combined['Time_Modelo without ANN'] = (df_combined['Time_Modelo'] - df_combined['ANN time_Modelo'])
-# Reorder the columns based on the desired order
-df_ordered = df_combined.reindex(columns=desired_order)
+# df_combined = pd.concat([df1.add_suffix('_Default'), df2.add_suffix('_Modelo')], axis=1)
+# desired_order = ['Time_Default', 'Time_Modelo','Time_Modelo without ANN', 'Nodes_Default','Nodes_Modelo','ANN time_Default','ANN time_Modelo']
+# df_combined['Time_Modelo without ANN'] = (df_combined['Time_Modelo'] - df_combined['ANN time_Modelo'])
+# # Reorder the columns based on the desired order
+# df_ordered = df_combined.reindex(columns=desired_order)
 
-print(df_ordered.to_latex(index=True,formatters={"name": str.upper},float_format="{:.3f}".format,bold_rows=True))
+# print(df_ordered.to_latex(index=True,formatters={"name": str.upper},float_format="{:.3f}".format,bold_rows=True))
 
 # columns=['File','Relative precision']
 # df_pre = pd.DataFrame(Criteria, columns=columns)
@@ -37,36 +37,38 @@ print(df_ordered.to_latex(index=True,formatters={"name": str.upper},float_format
 
 
 # # Time
-# average_time_Default = Default_grouped['Time'].mean()
-# average_time_HC1 = Criteria_grouped['Time'].mean()
-# df=average_time_Default.compare(average_time_HC1,result_names=('Default', 'MC-1'),keep_shape=True)
-# df['percentage_change'] = ((df['MC-1'] - df['Default']) / df['Default']) *100
-# df.name='Default vs MC-1: Time'
-# print('------------------------')
-# print(df.name)
-# print(df)
+average_time_Default = Default_grouped['Time'].mean()
+average_time_HC1 = Criteria_grouped['Time'].mean()
+df=average_time_Default.compare(average_time_HC1,result_names=('Default', 'MC-1'),keep_shape=True,keep_equal=True)
+df['percentage_change'] = ((df['MC-1'] - df['Default']) / df['Default']) *100
+df.name='Default vs MC-1: Time'
+print('------------------------')
+print(df.name)
+print(df.to_string())
 
 
 # #Nodes
-# avg_nodes_Default=Default_grouped['Nodes'].mean()
-# avg_nodes_MC1=Criteria_grouped['Nodes'].mean()
-# df2=avg_nodes_Default.compare(avg_nodes_MC1,result_names=('Default', 'MC-1'),keep_shape=True)
-# df2['percentage_change'] = ((df2['MC-1'] - df2['Default']) / df2['Default']) *100
-# df2.name='Default vs MC-1: Nodes'
-# print('------------------------')
-# print(df2.name)
-# print(df2)
+avg_nodes_Default=Default_grouped['Nodes'].mean()
+avg_nodes_MC1=Criteria_grouped['Nodes'].mean()
+df2=avg_nodes_Default.compare(avg_nodes_MC1,result_names=('Default', 'MC-1'),keep_shape=True,keep_equal=True)
+df2['percentage_change'] = ((df2['MC-1'] - df2['Default']) / df2['Default']) *100
+df2.name='Default vs MC-1: Nodes'
+print('------------------------')
+print(df2.name)
+print(df2.to_string())
 
 
 
-# avg_precision_Default=Default_grouped['Relative precision'].mean()
-# avg_precision_MC1=Criteria_grouped['Relative precision'].mean()
-# df3=avg_precision_Default.compare(avg_precision_MC1,result_names=('Default', 'MC-1'),keep_shape=True)
-# df3['percentage_change'] = ((df3['MC-1'] - df3['Default']) / df3['Default']) * 100
+avg_precision_Default=Default_grouped['Relative precision'].mean()
+avg_precision_MC1=Criteria_grouped['Relative precision'].mean()
+df3=avg_precision_Default.compare(avg_precision_MC1,result_names=('Default', 'MC-1'),keep_shape=True,keep_equal=True)
+print(df3.to_string())
+# df_combined = pd.concat([avg_precision_Default.add_suffix('_Default'), avg_precision_MC1.add_suffix('_Modelo')], axis=1)
+# df3['percentage_change'] = ((avg_precision_MC1['MC-1'] - avg_precision_Default['Default']) / avg_precision_Default['Default']) * 100
 # df3.name='Default vs MC-1: Precision'
 # print('------------------------')
 # print(df3.name)
-# print(df3)
+# print(df_combined.to_string())
 
 
 
--- ibexopt_multicore_solver.py
+++ ibexopt_multicore_solver.py
@@ -9,11 +9,11 @@ seed_Inicial=1
 seed_Final=4
 timeout=3600
 precision=0.001
-criteria="Model_MC-2_50%"
+criteria="Model_MC-4"
 path_to_benchs="/home/nico/codes/benchmarks/experiment_set.txt"
-# short='/home/nico/codes/benchmarks/short.txt'
+# results_tests='/home/nico/codes/benchmarks/positive_results.txt'
 ibexopt="/home/nico/Ibex/ibex-2.8.9/__build__/src/ibexopt"
-csv_file="benchmarks/MC-2/benchmark_{0}.csv".format(criteria)
+csv_file="benchmarks/base/benchmark_{0}.csv".format(criteria)
 # ---------------------------------------------
 # print("Number of cpu : ", multiprocessing.cpu_count())
 
@@ -23,8 +23,9 @@ def echo_sys(bench,seed_Inicial,seed_Final,timeout,ibexopt):
     for i in range(seed_Inicial,seed_Final+1):
         cmd="{0} {1} --random-seed={2} --timeout={3}".format(ibexopt,bench,i,timeout)
         output = subprocess.Popen( cmd, stdout=subprocess.PIPE, shell=True ).communicate()[0]
-        print(bench+" "+str(i)+" done")
+        # print(bench+" "+str(i)+" done")
         output=output.decode("utf-8")
+        print(output)
         output=output.split('\n') 
         results.append([bench,i,output[-2],timeout,output[-3],output[-4],output[-5],output[-6],output[-7]])
         
